2e94584c735c09a3732d7feeb9c57da77389b9b0
Edward Gilmore <edgilmore@live.com>
2018-02-21 16:51:02 -0700

    added implementation for the basic scoreboard layout and styles

--- score-board/src/App.js
+++ score-board/src/App.js
@@ -4,12 +4,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="heaer">
+      <div className="App scoreboard">
+        <div className="header">
           <h1>Scoreboard</h1>
         </div>
         <div className="players">
-          
+          <div className="player">
+            <div className="player-name">Player One</div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score">31</div>
+                <button className="counter-action increment"> + </button>
+              </div>
+            </div>
+          </div>
+          <div className="player">
+            <div className="player-name">Player Two</div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score">31</div>
+                <button className="counter-action increment"> + </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );

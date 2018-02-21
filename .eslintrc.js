4975cacaab0a636e8ffe4b91a079e1d521d7c7ad
Edward Gilmore <edgilmore@live.com>
2018-02-21 16:50:37 -0700

    add support for eslint react and import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'import', 'react'],
   rules: {
     'prettier/prettier': ['error'],
+    "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
+    "import/no-extraneous-dependencies": [
+      "error", {
+        "devDependencies": [
+          "dev.js",
+          "test/**",
+        ],
+        "optionalDependencies": false,
+        "peerDependencies": false
+      }
+    ]
   },
 };

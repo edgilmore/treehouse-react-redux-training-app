eee5e81df3fbc49d47c49e580fde318ad8ea82d2
Edward Gilmore <edgilmore@live.com>
2018-02-22 15:47:09 -0700

    imported the header component

--- score-board/src/App.js
+++ score-board/src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Player from './components/player';
+import Header from './components/header';
 import './App.css';
-
-class Header extends Component {
-  render() {
-    return(
-      <div className="header">
-        <h1>{this.props.title}</h1>
-      </div>
-    )
-  }
-}
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App scoreboard">
-        <Header title={this.props.title}/>
+        <Header title={this.props.title} />
         <div className="players">
           <Player name="Player One" />
           <Player name="Player Two" />
@@ -33,10 +20,10 @@ class App extends Component {
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 
 App.defaultProps = {
-  title: 'Scoreboard'
-}
+  title: 'Scoreboard',
+};
 
 export default App;

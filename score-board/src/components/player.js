2c9bb151faee0723a4854349d27fdd146591c758
Edward Gilmore <edgilmore@live.com>
2018-02-22 08:49:42 -0700

    added player component

---
+++ score-board/src/components/player.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Player extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+    };
+  }
+  render() {
+    return (
+      <div className="player">
+        <div className="player-name">{this.props.name}</div>
+        <div className="player-score">
+          <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score">{this.state.score}</div>
+            <button className="counter-action increment"> + </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Player;

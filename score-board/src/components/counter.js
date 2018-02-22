789d2e23a8a164d96f7df98d658c647834c7e095
Edward Gilmore <edgilmore@live.com>
2018-02-22 15:44:31 -0700

    created counter component

---
+++ score-board/src/components/counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+    };
+  }
+  incrementScore() {
+    this.setState({
+      score: (this.score += 1),
+    });
+  }
+  decrementScore() {
+    this.setState({
+      score: (this.score -= 1),
+    });
+  }
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score">{this.state.score}</div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = {
+  score: PropTypes.number.isRequired,
+}
+
+export default Counter;

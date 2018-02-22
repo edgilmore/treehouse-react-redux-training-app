fd4c17c882d0733f048185b5d7f459e0e83ae1ed
Edward Gilmore <edgilmore@live.com>
2018-02-22 15:28:39 -0700

    added proptype to the main app component

--- score-board/src/App.js
+++ score-board/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Player from './components/player';
 import './App.css';
 
@@ -16,6 +17,10 @@ class App extends Component {
       </div>
     );
   }
+}
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default App;

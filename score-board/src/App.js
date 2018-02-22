139f09adb5579d18716948ccd00a12dad7583ea9
Edward Gilmore <edgilmore@live.com>
2018-02-22 08:50:57 -0700

    removed static html and replaced with player component

--- score-board/src/App.js
+++ score-board/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Player from './components/player';
 import './App.css';
 
 class App extends Component {
@@ -6,29 +7,11 @@ class App extends Component {
     return (
       <div className="App scoreboard">
         <div className="header">
-          <h1>Scoreboard</h1>
+          <h1>{this.props.title}</h1>
         </div>
         <div className="players">
-          <div className="player">
-            <div className="player-name">Player One</div>
-            <div className="player-score">
-              <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <div className="counter-score">31</div>
-                <button className="counter-action increment"> + </button>
-              </div>
-            </div>
-          </div>
-          <div className="player">
-            <div className="player-name">Player Two</div>
-            <div className="player-score">
-              <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <div className="counter-score">31</div>
-                <button className="counter-action increment"> + </button>
-              </div>
-            </div>
-          </div>
+          <Player name="Player One" />
+          <Player name="Player Two" />
         </div>
       </div>
     );

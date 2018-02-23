5b5b14022245543d265f4797be290cf49c2499cb
Edward Gilmore <edgilmore@live.com>
2018-02-23 15:17:53 -0700

    added implementation for rsvp project

--- rsvp-app/src/App.js
+++ rsvp-app/src/App.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form action="">
+            <input type="text" value="" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">
+              Submit
+            </button>
+          </form>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label htmlFor="">
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li>
+              <span>Safia</span>
+            </li>
+            <li>
+              <span>Iver</span>
+              <label htmlFor="">
+                <input type="checkbox" checked="checked" /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li>
+              <span>Corrina</span>
+              <label htmlFor="">
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li>
+              <span>Joel</span>
+              <label htmlFor="">
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }

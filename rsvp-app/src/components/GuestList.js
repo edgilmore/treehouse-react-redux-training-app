ef781e1d899c412906f3988783267da420f35d64
Edward Gilmore <edgilmore@live.com>
2018-02-28 12:49:45 -0700

    refactor: implemented the guest list component

--- rsvp-app/src/components/GuestList.js
+++ rsvp-app/src/components/GuestList.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const GuestList = props => {};
+class GuestList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.guests.map((guest, index) => {
+          return <li key={index}>
+            <span>{guest.name}</span>
+            <label htmlFor="">
+              <input type="checkbox" checked={guest.isConfirmed} /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>;
+        })}
+      </ul>
+    );
+  }
+}
+
+GuestList.propTypes = {
+  guests: PropTypes.array.isRequired,
+};
 
 export default GuestList;

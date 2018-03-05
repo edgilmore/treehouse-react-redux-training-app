75b1fad2b0f8d93980236931d7e2a497f786f5e5
Edward Gilmore <edgilmore@live.com>
2018-03-05 15:30:31 -0700

    fix: updated the parser options and syntax fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,15 @@ module.exports = {
   plugins: ['prettier', 'import', 'react'],
   rules: {
     'prettier/prettier': ['error'],
-    "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
-    "import/no-extraneous-dependencies": [
-      "error", {
-        "devDependencies": [
-          "dev.js",
-          "test/**",
-        ],
-        "optionalDependencies": false,
-        "peerDependencies": false
-      }
-    ]
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['dev.js', 'test/**'],
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
   },
+  parser: 'babel-eslint'
 };

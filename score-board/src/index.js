c5ab428a32c03f8165a6b0c020c05bf320776b74
Edward Gilmore <edgilmore@live.com>
2018-05-08 19:47:16 -0700

    refactor: removed the constant from the index

--- score-board/src/index.js
+++ score-board/src/index.js
@@ -3,18 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const PLAYERS = [
-  {
-    id: 1,
-    name: 'Player One',
-    score: 0,
-  },
-  {
-    id: 2,
-    name: 'Player Two',
-    score: 0,
-  },
-];
-
-ReactDOM.render(<App title="My Scoreboard" players={PLAYERS} />, document.getElementById('root'));
-
+ReactDOM.render(<App title="My Scoreboard" />, document.getElementById('root'));

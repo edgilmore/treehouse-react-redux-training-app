7dd7708146a537d31b37dc8444a1f5d7ae8651af
Edward Gilmore <edgilmore@live.com>
2018-05-08 19:46:49 -0700

    chore: abstract players into intial state constant

---
+++ score-board/src/utils/constants.js
@@ -0,0 +1,16 @@
+const INTIAL_STATE = {
+  players: [
+    {
+      id: 1,
+      name: 'Player One',
+      score: 0,
+    },
+    {
+      id: 2,
+      name: 'Player Two',
+      score: 0,
+    },
+  ],
+};
+
+export default INTIAL_STATE;

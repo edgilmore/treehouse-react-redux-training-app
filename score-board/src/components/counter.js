f953f7e30a948f477d787c2ed35007e138205aa8
Gilmore, Edward (PLT9229) <edward.gilmore@usaa.com>
2018-06-01 17:32:24 -0700

    refactor: update function name

--- score-board/src/components/counter.js
+++ score-board/src/components/counter.js
@@ -4,17 +4,23 @@ import PropTypes from 'prop-types';
 export default class Counter extends Component {
   static propTypes = {
     score: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired,
+    updatePlayerScore: PropTypes.func.isRequired,
     playerId: PropTypes.number.isRequired,
   };
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.props.onChange(this.props.playerId, -1)}>
+        <button
+          className="counter-action decrement"
+          onClick={() => this.props.updatePlayerScore(this.props.playerId, -1)}
+        >
           -
         </button>
         <div className="counter-score">{this.props.score}</div>
-        <button className="counter-action increment" onClick={() => this.props.onChange(this.props.playerId, 1)}>
+        <button
+          className="counter-action increment"
+          onClick={() => this.props.updatePlayerScore(this.props.playerId, 1)}
+        >
           +
         </button>
       </div>
